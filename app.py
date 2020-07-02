1c161bb8a318cb867357e2bc9f23ee228904d04e
Swarnendu98 <swarnendu.dawn98@gmail.com>
2020-07-03 01:35:33 +0530

    create and update

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ class executive_data(db.Model):
     ex_id = db.Column(db.Integer,primary_key=True,autoincrement=True)
     ex_pass = db.Column(db.VARCHAR(25),nullable=False)
     ex_name = db.Column(db.String,nullable=False)
+
 class patient_data(db.Model):
     patient_id=db.Column(db.Integer,primary_key=True,autoincrement=True)
     patient_adhar= db.Column(db.BigInteger,unique=True)
@@ -33,12 +34,15 @@ class patient_data(db.Model):
     patient_address=db.Column(db.String,nullable=False)
     patient_state = db.Column(db.String,nullable=False)
     patient_city=db.Column(db.String,nullable=False)
+    patient_name=db.Column(db.Text,nullable=False)
+
 class pharmacy_data(db.Model):
     pharmacy_bill_no = db.Column(db.Integer,primary_key=True,autoincrement=True)
     patient_id=db.Column(db.Integer,nullable=False)
     medicine_name=db.Column(db.String,nullable=False)
     quantity = db.Column(db.Integer,nullable=False)
     price = db.Column(db.Float,nullable=False)
+
 class diagnosis_data(db.Model):
     diagnosis_bill_no=db.Column(db.Integer,primary_key=True,autoincrement=True)
     patient_id=db.Column(db.Integer,nullable=False)
@@ -64,6 +68,8 @@ def login():
             return render_template('login.html',message=message)
     else:
         return render_template('login.html')
+
+
 @app.route("/home")
 def home():
     if 'user_id' not in session:
@@ -74,9 +80,87 @@ def home():
         ex_name = ex_data.ex_name
 
         return render_template('home.html',name = ex_name)
+
+
 @app.route('/logout')
 def logout():
    # remove the username from the session if it is there
    session.pop('user_id', None)
    return redirect(url_for('login'))
+
+
+@app.route('/create_patient',methods=['GET','POST'])
+def create_patient():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    else:
+        if request.method == 'POST':
+            patient_adhar = request.form['patient_adhar']
+            patient_name = request.form['patient_name']
+            patient_age = request.form['patient_age']
+            patient_bed_type = request.form['patient_bed_type']
+            patient_address = request.form['patient_address']
+            patient_state = request.form['patient_state']
+            patient_city = request.form['patient_city']
+
+            new_patient = patient_data(patient_adhar=patient_adhar, patient_age=patient_age,
+                                       patient_bed_type=patient_bed_type,
+                                       patient_address=patient_address, patient_state=patient_state,
+                                       patient_city=patient_city,patient_name=patient_name)
+            db.session.add(new_patient)
+            db.session.commit()
+            message = 'Patient added Successfully'
+            return render_template("create_patient.html",message=message)
+        else:
+            #all_patient = patient_data.query.order_by(patient_data.date_posted).all()
+            return render_template('create_patient.html')
+
+@app.route('/update_patient',methods=['GET','POST'])
+def update_patient():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    else:
+        if request.method == 'POST' and 'search' in request.form:
+            search = request.form.get('search')
+            data= patient_data.query.filter_by(patient_adhar=search).first()
+            if data is None:
+                return render_template("update_patient.html",message="Patient does not exist")
+            else:
+                return render_template("update_patient.html",data = data )
+        elif request.method =='POST' and 'patient_adhar' in request.form:
+            patient_id =        request.form['patient_id']
+            patient_adhar =     request.form['patient_adhar']
+            patient_age =       request.form['patient_age']
+            patient_bed_type =  request.form['patient_bed_type']
+            patient_address =   request.form['patient_address']
+            patient_state =     request.form['patient_state']
+            patient_city =      request.form['patient_city']
+            patient_name =      request.form['patient_name']
+
+            update = patient_data.query.filter_by(patient_id = patient_id).first()
+
+            update.patient_adhar = patient_adhar
+            update.patient_age = patient_age
+            update.patient_bed_type = patient_bed_type
+            update.patient_address = patient_address
+            update.patient_state = patient_state
+            update.patient_city = patient_city
+            update.patient_name = patient_name
+
+            db.session.commit()
+
+            return render_template("update_patient.html", message="Patient Updated")
+        else:
+            return render_template("update_patient.html")
+
+
+
+
+
+
+
+
+
+
+
 
